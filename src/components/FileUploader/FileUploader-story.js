82e70cb364a41e9cfc575fafcbcd4f7401956c1f
amrios <amrios2817@gmail.com>
2018-03-08 17:39:25 -0500

    refactor(FileUploader): allow labelDescription to accept object or string

--- src/components/FileUploader/FileUploader-story.js
+++ src/components/FileUploader/FileUploader-story.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { FileUploader, FileUploaderButton } from 'carbon-components-react';
+import { FileUploaderButton } from 'carbon-components-react';
+import { FileUploader } from '../../index';
 import { withReadme } from 'storybook-readme';
 import readme from './README.md';
 
---
+++ src/components/FileUploader/FileUploader.js
@@ -0,0 +1,134 @@
+/* eslint react/no-multi-comp: "off" */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+// import { Tooltip } from '../../index';
+import { Filename, FileUploaderButton } from 'carbon-components-react';
+
+export default class FileUploader extends Component {
+  static propTypes = {
+    iconDescription: PropTypes.string,
+    buttonLabel: PropTypes.string,
+    buttonKind: PropTypes.oneOf(['primary', 'secondary']),
+    filenameStatus: PropTypes.oneOf(['edit', 'complete', 'uploading']).isRequired,
+    labelDescription: PropTypes.oneOf([PropTypes.object, PropTypes.string]),
+    labelTitle: PropTypes.string,
+    multiple: PropTypes.bool,
+    onChange: PropTypes.func,
+    onClick: PropTypes.func,
+    className: PropTypes.string,
+    accept: PropTypes.arrayOf(PropTypes.string),
+  };
+
+  static defaultProps = {
+    iconDescription: 'Provide icon description',
+    filenameStatus: 'uploading',
+    buttonLabel: '',
+    buttonKind: 'primary',
+    multiple: false,
+    onChange: () => {},
+    onClick: () => {},
+    accept: [],
+  };
+
+  state = {
+    filenames: [],
+    filenameStatus: '',
+  };
+
+  nodes = [];
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.filenameStatus !== this.props.filenameStatus) {
+      this.setState({ filenameStatus: nextProps.filenameStatus });
+    }
+  }
+  handleChange = evt => {
+    this.setState({ filenames: [...evt.target.files].map(file => file.name) });
+    this.props.onChange(evt);
+  };
+
+  handleClick = (evt, index) => {
+    const filteredArray = this.state.filenames.filter(
+      filename => filename !== this.nodes[index].innerText.trim(),
+    );
+    this.setState({ filenames: filteredArray });
+    this.props.onClick(evt);
+  };
+
+  clearFiles = () => {
+    // A clearFiles function that resets filenames and can be referenced using a ref by the parent.
+    this.setState({ filenames: [], filenameStatus: '' });
+  };
+
+  render() {
+    const {
+      iconDescription,
+      buttonLabel,
+      buttonKind,
+      filenameStatus,
+      labelDescription,
+      labelTitle,
+      className,
+      multiple,
+      accept,
+      ...rest
+    } = this.props;
+
+    const classes = classNames({
+      'bx--form-item': true,
+      [className]: className,
+    });
+
+    return (
+      <div className={classes} {...rest}>
+        <strong className="bx--label">{labelTitle}</strong>
+        <p className="bx--label-description">{labelDescription}</p>
+        <FileUploaderButton
+          labelText={buttonLabel}
+          multiple={multiple}
+          buttonKind={buttonKind}
+          onChange={this.handleChange}
+          disableLabelChanges
+          accept={accept}
+        />
+        <div className="bx--file-container">
+          {this.state.filenames.length === 0
+            ? null
+            : this.state.filenames.map((name, index) => (
+                <span
+                  key={index}
+                  className="bx--file__selected-file"
+                  ref={node => (this.nodes[index] = node)} // eslint-disable-line
+                  {...rest}
+                >
+                  <p className="bx--file-filename">{name}</p>
+                  <span className="bx--file__state-container">
+                    <Filename
+                      iconDescription={iconDescription}
+                      status={filenameStatus}
+                      onClick={evt => {
+                        if (filenameStatus === 'edit') {
+                          this.handleClick(evt, index);
+                        }
+                      }}
+                      iconDescription={iconDescription}
+                      onKeyDown={evt => {
+                        if (evt.which === 13 || evt.which === 32) {
+                          this.handleClick(evt, index);
+                        }
+                      }}
+                      onClick={evt => {
+                        if (filenameStatus === 'edit') {
+                          this.handleClick(evt, index);
+                        }
+                      }}
+                    />
+                  </span>
+                </span>
+              ))}
+        </div>
+      </div>
+    );
+  }
+}
