dd0822296db5303349c81090d6e4084228ad3bef
j1mie <james.loughran@gmail.com>
2018-04-10 21:03:20 +0100

    style(InteriorLeftNav): Remove open list from story

--- src/components/InteriorLeftNav/InteriorLeftNav-story.js
+++ src/components/InteriorLeftNav/InteriorLeftNav-story.js
@@ -11,7 +11,7 @@ storiesOf('Components|InteriorLeftNav', module)
     'default',
     withInfo(``)(() => (
       <InteriorLeftNav className="some-class">
-        <InteriorLeftNavList open={true} title="Start here">
+        <InteriorLeftNavList title="Start here">
           <InteriorLeftNavItem>
             <a target="_blank" href="#example-1">
               Developers
