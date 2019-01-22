e1fbe7fc52d5d5c6edb744ff26807130fcd75cd0
Matt Wong <Matthew.Wong1@ibm.com>
2019-01-22 16:05:46 -0500

    fix: overflow menu support for ie

--- src/globals/internal/FloatingMenu.js
+++ src/globals/internal/FloatingMenu.js
@@ -249,7 +249,7 @@ class FloatingMenu extends React.Component {
             refPosition,
             direction: menuDirection,
             offset,
-            scrollY: window.scrollY,
+            scrollY: window.pageYOffset,
           }),
         });
       }
